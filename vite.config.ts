4d0995429223dfe489d504d6aae17b492735a8d2
Jarrett Keifer <jkeifer@pdx.edu>
2026-03-26 11:09:03 -0700

    update config base

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/0x00c0dec5/',
   plugins: [react()],
   test: {
     globals: true,
